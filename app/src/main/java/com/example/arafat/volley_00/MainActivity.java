d47f0c4f00d244522c733358a32d7b6ff1efed6d
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-03-03 12:00:45 +0600

    Initial commit

---
+++ app/src/main/java/com/example/arafat/volley_00/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.arafat.volley_00;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private TextView text;
+    private Button get;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+        get = findViewById(R.id.btn);
+
+        get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Instantiate the cache
+                Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+                // Set up the network to use HttpURLConnection as the HTTP client.
+                Network network = new BasicNetwork(new HurlStack());
+
+                // Instantiate the RequestQueue with the cache and network.
+                final RequestQueue requestQueue;
+                requestQueue = new RequestQueue(cache, network);
+
+                // Start the queue
+                requestQueue.start();
+                String url = "http://192.168.43.30/volley.php";
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                text.setText(R.string.Response);
+                                text.append(response);
+                                requestQueue.stop();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //get.setText("That didn't work!");
+                        text.setText(error.toString());
+                        requestQueue.stop();
+                    }
+                });
+
+                // Add the request to the RequestQueue.
+                requestQueue.add(stringRequest);
+
+            }
+        });
+    }
+}
